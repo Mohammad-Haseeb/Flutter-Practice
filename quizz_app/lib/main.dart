420318e4a807ad75388bee2d98951c2dca82d4bb
Muhammad Haseeb Hussain <64950545+Mohammad-Haseeb@users.noreply.github.com>
2021-07-08 00:33:26 +0500

    button and question complete

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home:Scaffold(
-       backgroundColor: Colors.blueAccent,
-      body: MyApp(),
-    )
-  ));
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text("Quizz App"),
+      backgroundColor: Colors.cyan,
+    ),
+    backgroundColor: Colors.cyan[600],
+    body: MyApp(),
+  )));
 }
 
 class MyApp extends StatefulWidget {
@@ -21,7 +24,52 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("Hello"),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Center(
+                child: Container(
+                  child: const Text(
+                    "How're you",
+                    style: TextStyle(
+                        color: Colors.greenAccent,
+                        fontSize: 60,
+                        fontFamily: 'OtomanopeeOne'),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 240.0),
+              child: Column(
+                children: [
+                  FlatButton(
+                    onPressed: () {},
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 30, horizontal: 100),
+                      child: Text("True"),
+                    ),
+                    color: Colors.orange,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 30, horizontal: 100),
+                      child: Text("True"),
+                    ),
+                    color: Colors.orange,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
