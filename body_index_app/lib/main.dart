b351ef700b67098dd143d6948daabd6f37878dc3
Muhammad Haseeb Hussain <64950545+Mohammad-Haseeb@users.noreply.github.com>
2021-07-08 23:41:48 +0500

    Update main.dart

--- body_index_app/lib/main.dart
+++ body_index_app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Body Mass Index',
       theme: ThemeData(
-      
+
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
+ 
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
 
